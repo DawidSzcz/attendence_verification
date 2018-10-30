4889fcbe9e284d2d0598b490eb1cb0ed6f193ce7
Kazik <dawidszczyrk@gmail.com>
2018-10-30 02:57:49 +0100

    log

--- web/index.php
+++ web/index.php
@@ -4,6 +4,9 @@
 //defined('YII_DEBUG') or define('YII_DEBUG', true);
 //defined('YII_ENV') or define('YII_ENV', 'dev');
 
+echo 'index.php'; die;
+
+
 require(__DIR__ . '/../vendor/autoload.php');
 require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
